95714a9069f0266a321ace902e60cc5e5a03d0a7
Nikica Jokic <neektza@gmail.com>
2014-10-09 19:13:20 +0200

    Rubocop style recommendations for client_spec.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,12 +5,12 @@ describe ShippingApi::Client do
   describe 'initialization' do
     it 'checks provided arguments by using a struct' do
       expect do
-        c = ShippingApi::Client.new(is: 'this', nonsense: 'yes')
+        ShippingApi::Client.new(is: 'this', nonsense: 'yes')
       end.to raise_error(ArgumentError)
     end
   end
-  
-  describe "#config" do
+
+  describe '#config' do
     it 'provides access to config' do
       c = ShippingApi::Client.new(api_key: '238h4regnudfjls')
       expect(c.config).to be_a ShippingApi::Client::Configuration
