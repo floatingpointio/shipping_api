b89ae95c8f9906c4277f2728c725bfb6d04ce559
Veljko Dragsic <veljko@floatingpoint.io>
2014-10-11 18:35:33 +0200

    Endpoint example in client.
    Currently only to have something to do testing on.

--- lib/shipping_api/client.rb
+++ lib/shipping_api/client.rb
@@ -1,7 +1,10 @@
 require 'immutable_struct'
 require 'faraday'
 
+require 'response'
+
 module ShippingApi
+
   # Represents the Client
   class Client
     Configuration = ImmutableStruct.new(:api_url, :api_key)
@@ -12,6 +15,29 @@ module ShippingApi
       @config = Configuration.new(api_url, api_key)
       @conn = create_conn
     end
+
+    ### Endpoints ... testing only for now
+
+    # def get_shipment_status_by_shipment_id(id)
+    #   @conn.get 'GetShipmentStatusByShipmentId', ShipmentId: id
+    # end
+
+    # def get_shipment_status_by_ref1_and_pickup_date(ref1, pickup_date)
+    #   @conn.get 'GetShipmentStatusByRef1AndPickupDate', Ref1: ref1, PickupDate: pickup_date
+    # end
+
+    # def get_shipment_status_by_any_ref(ref_type, search_parameter)
+    #   @conn.get 'GetShipmentStatusByAnyRef', RefType: ref_type, SearchParameter: search_parameter
+    # end
+
+    # ...
+
+    def get_locations(name_or_postal)
+      res = @conn.get 'GetLocations', PostalCodeOrName: name_or_postal
+      Response.new res
+    end
+
+    # ...
 
     private
 
