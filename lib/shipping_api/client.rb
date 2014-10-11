7d672fb8a3e55192c31032f8193fe22edb94fd6d
Veljko Dragsic <veljko@floatingpoint.io>
2014-10-11 16:43:50 +0200

    Client#initialize params are api_url and api_key.

--- lib/shipping_api/client.rb
+++ lib/shipping_api/client.rb
@@ -3,10 +3,10 @@ require 'immutable_struct'
 module ShippingApi
   # Represents the Client
   class Client
-    Configuration = ImmutableStruct.new(:api_key)
+    Configuration = ImmutableStruct.new(:api_url, :api_key)
 
-    def initialize(api_key)
-      @config = Configuration.new(api_key)
+    def initialize(api_url, api_key)
+      @config = Configuration.new(api_url, api_key)
     end
     attr_reader :config
   end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,16 +2,18 @@ require 'shipping_api'
 
 describe ShippingApi::Client do
   describe '#config' do
+    let(:api_url) { 'http://web.service.com/api' }
     let(:api_key) { '238h4regnudfjls' }
 
     it 'provides access to config' do
-      c = ShippingApi::Client.new(api_key)
+      c = ShippingApi::Client.new(api_url, api_key)
       expect(c.config).to be_a ShippingApi::Client::Configuration
+      expect(c.config.api_url).to eq api_url
       expect(c.config.api_key).to eq api_key
     end
 
     it 'prevents changing config once client has been inited' do
-      c = ShippingApi::Client.new('something')
+      c = ShippingApi::Client.new(api_url, 'something')
       expect do
         c.config.api_key = 'else'
       end.to raise_error(NoMethodError)
