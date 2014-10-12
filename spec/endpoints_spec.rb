348aa8a544d8cb19acb032bed1f82d50ff8409d4
Veljko Dragsic <veljko@floatingpoint.io>
2014-10-12 17:38:28 +0200

    VCR configured to expire cassettes after 1 day.

--- spec/endpoints_spec.rb
+++ spec/endpoints_spec.rb
@@ -8,6 +8,10 @@ Dotenv.load
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.default_cassette_options = {
+    record: :once,
+    re_record_interval: 60 * 60 * 24 # 1 day
+  }
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = true
   c.ignore_localhost = true
